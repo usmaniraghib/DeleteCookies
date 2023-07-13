ddcf4a9e067105f168facc3f67a81a659013b777
usmaniraghib <javaprogrammers786@gmail.com>
2023-07-13 09:03:00 +0530

    ReferencedJars folder added

--- src/com/raghib/selenium/DeleteCookies.java
+++ src/com/raghib/selenium/DeleteCookies.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DeleteCookies {
 	public static void main(String[] args) {
 		// Set the driver path
-		System.setProperty("webdriver.chrome.driver", "C:\\SELENIUM\\browser-driver\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
@@ -14,5 +14,6 @@ public class DeleteCookies {
 		// click on any link
 		// login page- verify login url
 		driver.get("http://google.com");
+		driver.quit();
 	}
 }
